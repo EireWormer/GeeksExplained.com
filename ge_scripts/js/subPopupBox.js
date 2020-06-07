fcd501b52bcf90d12c15437299779211de639ba7
Anthony <anthonyrb@pm.me>
2020-06-07 17:12:47 +0100

    Added popup & form to subscribe button

---
+++ ge_scripts/js/subPopupBox.js
@@ -0,0 +1,11 @@
+function addToSubscriptionList() {
+  console.log("Success!");
+}
+
+function addSubscriberPopup() {
+  document.getElementById("sub-popup-bg").style.display = "initial";
+}
+
+function removeSubscriberPopup() {
+  document.getElementById("sub-popup-bg").style.display = "none";
+}
