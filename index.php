7168275e41a840f92d9996a2298d9ada21e7f67f
Anthony <anthonyrb@pm.me>
2020-06-20 11:04:47 +0100

    created static article summary card

--- index.php
+++ index.php
@@ -91,7 +91,18 @@ if(isset($_COOKIE['GeeksExplainedAnalytics'])) {
 
         <main>
             <article class="all_posts">
-                <article class="post">
+                <article class="summary-card">
+                    <div class="summary-card-title-container"><a href="">Power supply (and all it's cables)</a></div>
+                    <div class="summary-card-artice-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean blandit sem felis, sit amet porttitor urna commodo nec. Vivamus mattis felis lectus. Nulla ornare ni...</div>
+                    <div class="summary-card-links"><a href="./article/2020/06/19/power-supply-and-all-its-cables">Read more</a></div>
+                </article>
+            </article>
+
+            <article class="all_posts">
+                <article class="summary-card">
+                    <div class="summary-card-title-container"><a href="">Power supply (and all it's cables)</a></div>
+                    <div class="summary-card-artice-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean blandit sem felis, sit amet porttitor urna commodo nec. Vivamus mattis felis lectus. Nulla ornare ni...</div>
+                    <div class="summary-card-links"><a href="./article/2020/06/19/power-supply-and-all-its-cables">Read more</a></div>
                 </article>
             </article>
         </main>
