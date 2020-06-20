0304432d6c54da16ea998cef2855a474cc7bb5b9
Anthony <anthonyrb@pm.me>
2020-06-20 11:46:49 +0100

    Fixed title link bug

--- ge_scripts/php/print_article_summary.php
+++ ge_scripts/php/print_article_summary.php
@@ -2,7 +2,7 @@
 function printArticleSummary($title, $summary, $link) {
     echo '
     <article class="summary-card">
-        <div class="summary-card-title-container"><a href="">' . $title . '</a></div>
+        <div class="summary-card-title-container"><a href="' . $link . '">' . $title . '</a></div>
         <!-- 167 max -->
         <div class="summary-card-artice-summary">' . $summary . '...</div>
         <div class="summary-card-links"><a href="' . $link . '">Read more</a></div>
